02a70fa499bba6f089d600427626e97c8ab7b8eb
Jian <kongjian.work@gmail.com>
2018-06-20 16:48:42 -0600

    delete all old stuff, copy to client and server

--- server/src/main/java/com/lightbend/akka/sample/AkkaQuickstart.java
+++ server/src/main/java/com/lightbend/akka/sample/AkkaQuickstart.java
@@ -3,30 +3,22 @@ package com.lightbend.akka.sample;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import com.lightbend.akka.sample.Greeter.*;
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 import java.io.IOException;
 
 public class AkkaQuickstart {
-
-  public static final int PORT = 12345;//监听的端口号
   public static void main(String[] args) {
     final ActorSystem system = ActorSystem.create("helloakka");
     try {
       //#create-actors
-      final ActorRef printerActor =
-              system.actorOf(Printer.props(), "printerActor");
-      final ActorRef howdyGreeter =
-              system.actorOf(Greeter.props("Howdy", printerActor), "howdyGreeter");
-      final ActorRef helloGreeter =
-              system.actorOf(Greeter.props("Hello", printerActor), "helloGreeter");
-      final ActorRef goodDayGreeter =
-              system.actorOf(Greeter.props("Good day", printerActor), "goodDayGreeter");
+      final ActorRef printerActor = 
+        system.actorOf(Printer.props(), "printerActor");
+      final ActorRef howdyGreeter = 
+        system.actorOf(Greeter.props("Howdy", printerActor), "howdyGreeter");
+      final ActorRef helloGreeter = 
+        system.actorOf(Greeter.props("Hello", printerActor), "helloGreeter");
+      final ActorRef goodDayGreeter = 
+        system.actorOf(Greeter.props("Good day", printerActor), "goodDayGreeter");
       //#create-actors
 
       //#main-send-messages
@@ -48,62 +40,6 @@ public class AkkaQuickstart {
     } catch (IOException ioe) {
     } finally {
       system.terminate();
-    }
-    System.out.println("服务器启动...\n");
-    AkkaQuickstart server = new AkkaQuickstart();
-    server.init();
-  }
-
-  public void init() {
-    try {
-      ServerSocket serverSocket = new ServerSocket(PORT);
-      while (true) {
-        // 一旦有堵塞, 则表示服务器与客户端获得了连接
-        Socket client = serverSocket.accept();
-        // 处理这次连接
-        new HandlerThread(client);
-      }
-    } catch (Exception e) {
-      System.out.println("服务器异常: " + e.getMessage());
-    }
-  }
-
-  private class HandlerThread implements Runnable {
-    private Socket socket;
-    public HandlerThread(Socket client) {
-      socket = client;
-      new Thread(this).start();
-    }
-
-    public void run() {
-      try {
-        // 读取客户端数据
-        DataInputStream input = new DataInputStream(socket.getInputStream());
-        String clientInputStr = input.readUTF();//这里要注意和客户端输出流的写方法对应,否则会抛 EOFException
-        // 处理客户端数据
-        System.out.println("客户端发过来的内容:" + clientInputStr);
-
-        // 向客户端回复信息
-        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-        System.out.print("请输入:\t");
-        // 发送键盘输入的一行
-        String s = new BufferedReader(new InputStreamReader(System.in)).readLine();
-        out.writeUTF(s);
-
-        out.close();
-        input.close();
-      } catch (Exception e) {
-        System.out.println("服务器 run 异常: " + e.getMessage());
-      } finally {
-        if (socket != null) {
-          try {
-            socket.close();
-          } catch (Exception e) {
-            socket = null;
-            System.out.println("服务端 finally 异常:" + e.getMessage());
-          }
-        }
-      }
     }
   }
 }
