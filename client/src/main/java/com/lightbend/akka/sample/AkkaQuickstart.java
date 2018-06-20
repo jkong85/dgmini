8dbca529f51afa2e629f291f0c6fb8fca42140c2
Jian <kongjian.work@gmail.com>
2018-06-20 17:28:51 -0600

    change language

--- client/src/main/java/com/lightbend/akka/sample/AkkaQuickstart.java
+++ client/src/main/java/com/lightbend/akka/sample/AkkaQuickstart.java
@@ -11,8 +11,8 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class AkkaQuickstart {
-  public static final String IP_ADDR = "localhost";//服务器地址
-  public static final int PORT = 12345;//服务器端口号
+  public static final String IP_ADDR = "localhost";//server address
+  public static final int PORT = 12345;// server port
   public static void main(String[] args) {
     final ActorSystem system = ActorSystem.create("helloakka");
     try {
@@ -48,7 +48,7 @@ public class AkkaQuickstart {
       system.terminate();
     }
 
-    System.out.println("客户端启动...");
+    System.out.println("Client is starting ...");
     System.out.println("当接收到服务器端字符为 \"OK\" 的时候, 客户端将终止\n");
     while (true) {
       Socket socket = null;
@@ -60,15 +60,15 @@ public class AkkaQuickstart {
         DataInputStream input = new DataInputStream(socket.getInputStream());
         //向服务器端发送数据
         DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-        System.out.print("请输入: \t");
+        System.out.print("Please input : \t");
         String str = new BufferedReader(new InputStreamReader(System.in)).readLine();
         out.writeUTF(str);
 
         String ret = input.readUTF();
-        System.out.println("服务器端返回过来的是: " + ret);
+        System.out.println("Server answer : " + ret);
         // 如接收到 "OK" 则断开连接
         if ("OK".equals(ret)) {
-          System.out.println("客户端将关闭连接");
+          System.out.println("Client will be closed ");
           Thread.sleep(500);
           break;
         }
@@ -76,7 +76,7 @@ public class AkkaQuickstart {
         out.close();
         input.close();
       } catch (Exception e) {
-        System.out.println("客户端异常:" + e.getMessage());
+        System.out.println("Client has exception:" + e.getMessage());
       } finally {
         if (socket != null) {
           try {
